3ba23b09bb2d7f8bfc9857ea2026cc752128dba8
alvarezleiva <galvarezleiv@gmail.com>
2024-11-14 09:35:58 +0100

    mostramos el nombre mas pequeño y el primero en orden alfabético

--- Reto3/src/Reto3/funcionesG.java
+++ Reto3/src/Reto3/funcionesG.java
@@ -10,20 +10,32 @@ public class funcionesG {
 
 		Scanner sc = new Scanner(System.in);
 
-		String s = "";
-		int len = s.length();
-		int meses = (int) ChronoUnit.MONTHS.between(fecha1, fecha2);
+		String pequeno = "";
+		String alfabetico = "";
 
-		System.out.println("Introduce algunas frases");
+		String s = "";
+		int len = 0;
+		int meses = (int) ChronoUnit.MONTHS.between(fecha1, fecha2);
+		
+		
+
+		
 		if (meses < 3) {
 			for (int i = 0; i < 4; i++) {
+				System.out.println("Introduce nombre");
 				s = sc.nextLine();
-		
 				
+				//Valor más pequeño
+				if( len == 0) {
+					pequeno = s;}
 			}
-			System.out.println(s);
+				//Alfabéticamente primero
+			if(s.charAt(0) == 'a') {
+				alfabetico = s;
+			}
 		}
-		
 
+		System.out.println("El más pequeño es " + pequeno);
+		System.out.println("El que alfabéticamente va primero es " + alfabetico);
 	}
 }
