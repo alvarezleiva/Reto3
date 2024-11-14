be66b437346d88318dab785c321ec0141d153121
alvarezleiva <galvarezleiv@gmail.com>
2024-11-14 08:47:03 +0100

    empezamos a crear la primera función, la del mismo año

--- Reto3/src/Reto3/Reto3.java
+++ Reto3/src/Reto3/Reto3.java
@@ -1,23 +1,27 @@
 package Reto3;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+
 public class Reto3 {
 
 	public static void main(String[] args) {
-		
-	Scanner sc = new Scanner(System.in);
-	
-	LocalDate fecha1 = Funciones.dimeFecha("Escribe una fecha", sc);
-	
-	LocalDate fecha2 = Funciones.dimeFecha("Escribe otra fecha", sc);
 
-	
-		if(fecha2.isBefore(fecha1)) {
+		Scanner sc = new Scanner(System.in);
+
+		LocalDate fecha1 = Funciones.dimeFecha("Escribe una fecha", sc);
+
+		LocalDate fecha2 = Funciones.dimeFecha("Escribe otra fecha", sc);
+
+		if (fecha2.isBefore(fecha1)) {
 			fecha2 = Funciones.dimeFecha("Escribe otra fecha que sea mayor que la primera", sc);
 		}
+		System.out.println(fecha1);
+		System.out.println(fecha2);
 
 	}
+	
 
 
 }
---
+++ Reto3/src/Reto3/funcionesG.java
@@ -0,0 +1,24 @@
+package Reto3;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+public class funcionesG {
+
+	public static void mismoaño(LocalDate fecha1, LocalDate fecha2) {
+		
+		LocalDate primerafecha;
+		LocalDate segundafecha;
+		
+		int dias = (int) ChronoUnit.DAYS.between(fecha1,fecha2);
+		int meses = (int) ChronoUnit.MONTHS.between(fecha1, fecha2);
+		int años = (int) ChronoUnit.YEARS.between(fecha1, fecha2);
+		
+		
+
+		
+		
+		
+		
+	}
+}
